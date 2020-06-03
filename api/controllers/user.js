c38dbbd3edcca7a8cd4a4c7f4d5700a95d0099a5
Daniel Alves <daniel@wedapp.io>
2020-06-02 22:51:51 -0400

    updating user data method

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -122,11 +122,29 @@ function getUsers(req, res) {
     });
 }
 
+// Actualizar datos de usuario
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+    // borrar propiedad password
+    delete update.password;
+    // comprobar que el user id es diferente del usuario identificado
+    if(userId != req.user.sub){
+        return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+    }
+    User.findByIdAndUpdate(userId, update, {new: true}, (err, userUpdated) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+        if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+        return res.status(200).send({user: userUpdated});
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
     getUser,
-    getUsers
+    getUsers,
+    updateUser
 }
