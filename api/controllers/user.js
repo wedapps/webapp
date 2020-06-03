5330daae2bfd463111ee879852ff03ecd59d5882
Daniel Alves <daniel@wedapp.io>
2020-06-02 22:32:26 -0400

    pagination users

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,6 +3,7 @@
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
+var mongoosePaginate = require('mongoose-pagination');
 
 // Métodos de Pruebas
 function home (req, res) {
@@ -102,10 +103,30 @@ function getUser(req, res) {
     });
 }
 
+// Devolver un listado de usuarios paginado
+function getUsers(req, res) {
+    var identity_user_id = req.user.sub;
+    var page = 1;
+    if(req.params.page) {
+        page = req.params.page;
+    }
+    var itemsPerPage = 5;
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if(err) return res.status(500).send({message: 'Error en la petición de usuario'});
+        if(!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total/itemsPerPage)
+        });
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
-    getUser
+    getUser,
+    getUsers
 }
